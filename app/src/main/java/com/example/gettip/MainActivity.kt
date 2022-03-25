9b259454c7ac2e48a398af4e358af9044bff3885
Yashraj Jain <yashrajjain726@gmail.com>
2022-03-25 19:25:07 +0530

    theme, responsiveness and scroll on rotate

--- app/src/main/java/com/example/gettip/MainActivity.kt
+++ app/src/main/java/com/example/gettip/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.gettip
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.gettip.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -14,9 +18,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.calculateButton.setOnClickListener{calculateTip()}
-
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode,_ -> handleKeyEvent(view,keyCode) }
     }
-
      fun calculateTip() {
          val stringInTextField = binding.costOfServiceEditText.text.toString()
          val cost = stringInTextField.toDoubleOrNull()
@@ -38,5 +41,14 @@ class MainActivity : AppCompatActivity() {
          }
          val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
          binding.tipResult.text = getString(R.string.tip_amount,formattedTip)
+    }
+    private fun handleKeyEvent(view: View,keyCode:Int):Boolean{
+        if(keyCode == KeyEvent.KEYCODE_ENTER)
+        {
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
+            return true
+        }
+        return false
     }
 }
